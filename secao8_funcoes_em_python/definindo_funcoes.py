2851e375214b8b3455894b0edbfbf293c7fcf4d8
dennis.gusmao <dennis.gusmao@aquare.la>
2023-01-30 18:06:19 -0300

    Update: definindo funções

--- secao8_funcoes_em_python/definindo_funcoes.py
+++ secao8_funcoes_em_python/definindo_funcoes.py
@@ -34,3 +34,63 @@ print(cores)
 
 cores.clear()
 print(cores)
+
+
+# DRY - Don't Repeat Yourself - Não repita você mesmo / Não repita seu código
+
+# Mas então, como definir funções?
+""" 
+Em Python, a forma geral de definir uma função é:
+
+def nome_da_funcao(parametros_de_entrada):
+    bloco_da_funcao
+
+Onde:
+
+nome_da_funcao -> SEMPRE, com letras minusculas, e se for nome composto, separado por underline (Snake Case)
+parametros_de_entradas -> São opicionais, onde tendo mais de um, cada um separado por vírgula, podendo ser opicionais ou não:
+bloco_da_funcao -> Também chamado de corpo da função ou implementação, é onde o processamento da função acontece.
+Neste bloco, pode ter ou não retorno da função.
+
+OBS: Veja que para definir uma função, utilizamos a palavra reservada 'def' informando ao Python que 
+estamos definindo uma função. Também abrimos o bloco de código com o já cenhecimento dois pontos :
+que é utilizado em Python para definir blocos.
+
+"""
+
+# Definindo a primeiro função:
+
+def diz_oi():
+    print('oi')
+    
+""" 
+Obs:
+1 - Veja que, dentro da nossa função podemes utilizar outra função;
+2 - Veja que nossa função executa uma tarefa, ou seja a única coisa que ela faz é dizer oi;
+3 - Veja que está função não recebe nenhum parametro de entrada;
+4 - Veja que está função não retorna nada;
+
+"""
+# Utilizando funções:
+# chamada de execução
+diz_oi()
+# OBS: ATENÇÃO, nunca esqueca de utilizar o parenteses ao executar uma função.
+
+
+# Exemplo de função 2
+def cantar_parabens():
+    print('Parabéns para você')
+    print('Nesta data querida')
+    print('Muitas felicidades para você')
+    
+cantar_parabens()
+cantar_parabens()
+
+for n in range(4):
+    cantar_parabens()
+    
+# Em Python podemos incluse criar variaveis do tipo de uma função e executar está função atraves da variavel 
+
+canta = cantar_parabens # aqui é sem o parenteses
+
+canta()
