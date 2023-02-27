bbbc84627ef1f5e305f0194f2a317d178f496437
Dennisaugus <dennisasg@outlook.com>
2023-02-27 08:36:58 -0300

    Aula11: tipos de erros

---
+++ secao11_debugando_e_tratando_erros/erros_mais_comuns.py
@@ -0,0 +1,85 @@
+""" 
+Erros mais comuns no Python
+
+ATENÇÃO:
+É importante prestar atenção e aprender a ler as saidas de erros geradas pela execução
+do nosso codigo 
+
+Os erros mais comuns:
+
+1 - SyntaxError -> Ocorre quando o python encontra um erro de sintaxe. Ou seja, você escreveu algo que 
+o Python não reconhece como parte da linguagem.
+
+# Exemplos SyntaxError
+a)
+def funcao:
+    print('Dennis ')
+
+b)
+None = 1
+def = 1
+
+c)
+return
+
+
+2 - NameError -> Ocorre quando uma variavel ou função não foi definida.
+
+# Exemplos de NameError
+a)
+print(dennis)
+
+b)
+funcao()
+
+c) 
+a = 18
+if a < 10:
+    msg = 'É maior que 10'
+print(msg)
+
+
+3 - TypeError -> Ocorre quando uma função/operação/ação é aplicada a um tipo erro 
+
+# Exemplos de TypeError
+a)
+print(len(5))
+
+b)
+print('Dennis' + [])
+print('Dennis' + 4)
+
+
+4 - IndexError -> Ocorre quando tentamos acessar um elemento em uma lista ou outro tipo de dado indexado utilizando
+um índice inválido.
+
+# Exemplos de IndexError 
+a)
+lista = ['Dennis']
+print(lista[2])
+print(lista[0][10])
+
+b)
+tupla = ('Dennis',)
+print(tupla[0][10])
+
+
+5 - ValueError -> Ocorre quando uma função ou operação built-in (integrada) recebe um argumento com o tipo
+correto mas valor inapropriado.
+
+# Exemplos de ValueError
+print(int('Dennis'))
+
+
+6 - KeyError -> Ocorre quando tentamos acessar um dicionário com uma chave que não existe.
+# Exemplos KeyError
+
+a)
+dict = {}  #vai dar certo (dict = {'dennis': 'meu nome'})
+print(dict['Dennis'])
+
+"""
+
+
+
+
