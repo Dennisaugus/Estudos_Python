8c4f98ce073fbbb9dfb1a57e40040bf22a4bc7cf
dennis.gusmao <dennis.gusmao@aquare.la>
2023-03-08 10:04:32 -0300

    Aula12 modulos externos

---
+++ secao12_trabalhando_com_modulos_e_pacotes_em_python/modulos_externos.py
@@ -0,0 +1,22 @@
+""" 
+Módulos Externos
+
+Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Package
+Você pode conhecer todos os pacotes externos oficiais em: https://pypi.org
+
+pip install --upgrade pip -> atualiza os pacotes do pip
+
+colorama -> é utilizado para permitir impressão de textos coloridos no terminal.
+"""
+#Utilizando o pacote instalado 
+# pip install colorama
+"""
+from colorama import init,Fore 
+init()
+print(Fore.BLACK + 'Dennis')
+print(Fore.BLUE + 'Dennis Augusto')
+"""
+import pydf
+pdf = pydf.generate_pdf('<h1> Dennis</h1>')
+with open('test_doc.pdf','wb') as f:
+    f.write(pdf)
