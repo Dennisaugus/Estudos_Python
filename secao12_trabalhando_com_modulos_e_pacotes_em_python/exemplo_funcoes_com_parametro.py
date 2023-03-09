cfe0d0e7ed95507078f72433646aacab27da21a4
dennis.gusmao <dennis.gusmao@aquare.la>
2023-03-09 08:25:03 -0300

    Aula12 Dunder Main e Name

---
+++ secao12_trabalhando_com_modulos_e_pacotes_em_python/exemplo_funcoes_com_parametro.py
@@ -0,0 +1,17 @@
+def soma_impares(numeros):
+    total = 0
+    for num in numeros:
+        if num % 2 != 0:
+            total += num
+    return total
+
+#utilizando o dunder 
+
+if __name__ == '__main__':
+    lista = [1,2,3,4,5,6,7]
+    print(soma_impares(lista))
+
+    tupla = (1,2,3,4,5,6,7)
+    print(soma_impares(tupla))
+else:
+    print('O módulo funções com parametro foi importado')
